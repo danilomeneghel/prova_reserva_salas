3878e2cfdf8d14e6e92c4ba662a15c95ada4c192
danilomeneghel <danilo.meneghel@gmail.com>
2016-05-10 22:55:48 -0300

    Adicionado a lib datetimepicker para aparecer o calendário com a data e hora no formulário de cadastro

--- views/reservasala/_form.php
+++ views/reservasala/_form.php
@@ -5,6 +5,8 @@ use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
 use app\models\Usuario;
 use app\models\Sala;
+use kartik\datetime\DateTimePicker;
+
 //use kartik\datecontrol\Module;
 //use kartik\datecontrol\DateControl;
 
@@ -20,44 +22,48 @@ use app\models\Sala;
     <div class="panel panel-default">
         <div class="panel-heading"><h4><i class="glyphicon glyphicon-user"></i> Dados da Reserva de Sala</h4></div>
         <div class="panel-body">
-            
+
                 <div class="row">
                     <div class="col-sm-4">
                         <?= $form->field($model, 'idUsuario')->dropDownList(ArrayHelper::map(Usuario::find()->all(), 'idUsuario', 'nome'))->label("Nome") ?>
                     </div>
                 </div>
-            
+
                 <div class="row">
                     <div class="col-sm-4">
                         <?= $form->field($model, 'idSala')->dropDownList(ArrayHelper::map(Sala::find()->all(), 'idSala', 'nro_sala'))->label("Nro Sala") ?>
                     </div>
                 </div>
-                
+
                 <div class="row">
                     <div class="col-sm-4">
-                        <?php 
-                            echo $form->field($model, 'periodo')->textInput(['maxlength' => true]);
-                            echo " (yyyy-mm-dd H:i:s)";
-                        ?>
-                        <?php 
-                        /*echo $form->field($model, 'periodo')->widget(DateControl::classname(), [
-                            'type'=>DateControl::FORMAT_DATETIME
-                        ]);*/
+                        <?php
+                            echo '<label>Período</label>';
+                            echo DateTimePicker::widget([
+                                'name' => 'Reservasala[periodo]',
+                                'options' => ['placeholder' => 'Selecione o Período'],
+                                'removeButton' => false,
+                                'pluginOptions' => [
+                                    'format' => 'yyyy-mm-dd H:i:s',
+                                    'autoclose' => true,
+                                    'language' => 'pt',
+                                ]
+                            ]);
                         ?>
                     </div>
                 </div>
-                
+
                 <div class="row">&nbsp;</div>
-            
+
                 <div class="row">
                     <div class="col-sm-4">
                         <?= $form->field($model, 'duracao')->textInput(['readonly' => true, 'value' => '1:00']) ?>
                     </div>
                 </div>
-    
+
         </div>
     </div>
-    
+
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Adicionar' : 'Alterar', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
